961667c86ddae7a9f129f3054159ba6bde9f041c
tobijkl <tobijkl@users.noreply.github.com>
2016-08-18 23:38:30 +0200

    fixed, works now with api v3

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 javascript:(function(){
 
-var api_key = "api-key-goes-here";
 var api_user = "api-user-goes-here";
+var api_key = "api-key-goes-here";
+
 var q=location.href;
+var data = '{"text":"'+q+'","type":"todo","value":"0"}';
+
 var xhr = new XMLHttpRequest();
 
-xhr.open("POST", "https://habitrpg.com:443/api/v2/user/tasks", true);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("x-api-key", api_key);
-xhr.setRequestHeader("x-api-user", api_user);
-var data = '{"text":"'+q+'","type":"todo","value":"0"}';
 xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
+    if(this.readyState == 4 && this.status == 201) {
         window.alert("Task created.");
     }
 };
+
+xhr.open("POST", "https://habitica.com/api/v3/tasks/user", true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("x-api-key", api_key);
+xhr.setRequestHeader("x-api-user", api_user);
 xhr.send(data);
 
 })();
